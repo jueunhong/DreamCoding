742821547a8d5338a5673b98ea64324d52369753
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-09-25 01:21:57 +0900

    script async, defer, variable 정리

---
+++ javascript/variable.js
@@ -0,0 +1,42 @@
+'use strict';
+
+//2. variable
+//let
+
+{let name = 'jueun';
+console.log(name);
+name = 'ellie';
+console.log(name);}
+
+//3. variable types
+//number
+const infinity = 1/0
+  
+const negativeInfinity =-1/0
+  
+const nAn = 'not a number'/2
+
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+//string
+const char = 'c';
+const brendan = 'brendan';
+const greeting ="hello " + brendan;
+console.log(`value= ${greeting}, type=${typeof greeting}`); //template literals
+const helloBob = `hi ${brendan}`;
+console.log(`value= ${helloBob}, type=${typeof helloBob}`);
+
+//boolean
+const bye = true;
+const falsetest = 1>3;
+console.log(`value= ${bye}, type=${typeof bye}`);
+console.log(`value= ${falsetest}, type=${typeof falsetest}`);
+
+//null
+
+//undefined
+
+//symbol
+
